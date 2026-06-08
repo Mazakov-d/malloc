58e42b028f9c3b20492100a723920f1d0e6f3660
Mazakov-d <mazaridorian5@gmail.com>
2026-06-08 13:42:03 +0200

    restructuring the project

--- src/malloc.c
+++ src/malloc.c
@@ -3,7 +3,7 @@
 
 typedef struct s_memory_chunk {
 	size_t					size;
-	t_page					*page;
+	t_page					**page;
 	struct s_memory_chunk	*next;
 	struct s_memory_chunk	*prev;
 } t_memory_chunk;
@@ -58,72 +58,75 @@ void	create_new_ctx()
 	return ;
 }
 
-void	add_new_page(size_t size)
+t_memory_chunk	*add_new_page(size_t size)
 {
 	void	*addr;
 	t_page	*new_page;
 	t_page	*previous_page;
+	t_memory_chunk	*free_memory;
 
 	if (size <= 4096 - get_chunk_size(sizeof(t_page) - get_chunk_size(sizeof(t_memory_chunk))))
 		size = 4096;
 	addr = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if (addr == (void*)-1)
-		return ;
+		return NULL;
 	new_page = addr;
+	new_page->addr = addr;
+	new_page->page_size = size;
+	new_page->used_memory_chunk = NULL;
 	previous_page = ctx->first_page;
 	while (previous_page->next)
 		previous_page = previous_page->next;
 	new_page->prev = previous_page;
 	previous_page->next = new_page;
 	new_page->next = NULL;
-	new_page->allocated_memory_chunk = NULL;
-	new_page->free_memory_chunk = addr + get_chunk_size(sizeof(t_page));
-	new_page->free_memory_chunk->next = NULL;
-	new_page->free_memory_chunk->prev = NULL;
-	new_page->free_memory_chunk->size = size - get_chunk_size(sizeof(t_page)) - get_chunk_size(sizeof(t_memory_chunk));
+	free_memory = addr + get_chunk_size(sizeof(t_page));
+	free_memory->next = NULL;
+	free_memory->size = size - get_chunk_size(sizeof(t_page)) - get_chunk_size(sizeof(t_memory_chunk));
+	free_memory->page = &new_page->addr;
+	return free_memory;
 }
 
 void	*find_enough_free_memory(size_t size)
 {
-	void	*save_ptr;
+	t_memory_chunk	*ptr;
 
-	save_ptr = ctx->first_page->free_memory_chunk;
-	while (ctx->first_page->free_memory_chunk)
+	ptr = ctx->free_memory_chunk;
+	while (ptr)
 	{
-		if (!ctx->first_page->free_memory_chunk->next)
+		if (!ptr)
 			break;
-		if (ctx->first_page->free_memory_chunk->size < size)
-			ctx->first_page->free_memory_chunk = ctx->first_page->free_memory_chunk->next;
+		if (ptr->size < size)
+			ptr = ptr->next;
 		else
 			break;
 	}
-	if (ctx->first_page->free_memory_chunk->size <= get_chunk_size(size))
-		add_new_page(size);
-
+	if (!ptr->size <= get_chunk_size(size))
+		ptr = add_new_page(size);
+	return ptr;
 }
 
 void	*allocate_memory(size_t size)
 {
-	size_t	free_size;
-	size_t	needed_memory_size;
-	void	*memory_chunk;
+	size_t						free_size;
+	size_t						needed_memory_size;
+	t_memory_chunk		*memory_chunk;
+	t_page						*actual_page;
 
 	needed_memory_size = get_chunk_size(sizeof(t_memory_chunk)) + get_chunk_size(size);
 	memory_chunk = find_enough_free_memory(needed_memory_size);
 	if (!memory_chunk)
 		return NULL;
 
-	if (ctx->first_page->allocated_memory_chunk)
+	actual_page = *memory_chunk->page;
+	if (actual_page->used_memory_chunk)
 	{
-		ctx->first_page->allocated_memory_chunk->next = ctx->first_page->free_memory_chunk;
-		ctx->first_page->allocated_memory_chunk->next->prev = ctx->first_page->allocated_memory_chunk;
-		ctx->first_page->allocated_memory_chunk = ctx->first_page->allocated_memory_chunk->next;
+			actual_page->used_memory_chunk->next = memory_chunk;
+			//find last node and make prev next relation
 	}
 	else
-		ctx->first_page->allocated_memory_chunk = ctx->first_page->free_memory_chunk;
-	ctx->first_page->free_memory_chunk += needed_memory_size;
-	ctx->first_page->free_memory_chunk->size = free_size - needed_memory_size;
-	return (ctx->first_page->allocated_memory_chunk + get_chunk_size(sizeof(t_memory_chunk)));
+		actual_page->used_memory_chunk = memory_chunk;
+	return (memory_chunk + get_chunk_size(sizeof(t_memory_chunk)));
 }
 
 void	*ft_malloc(size_t size)
@@ -147,7 +150,7 @@ void	*ft_malloc(size_t size)
 			if (working_ptr->next)
 				working_ptr->next->prev = working_ptr->prev;
 			if (working_ptr->prev)
-				working_ptr->prev = working_ptr->next;
+				working_ptr->prev->next = working_ptr->next;
 			save = working_ptr->next;
 			munmap(working_ptr->addr, working_ptr->page_size);
 			working_ptr = save;
