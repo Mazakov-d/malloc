b08e11681c3d5698329233b645d0e0e4099f225c
Mazakov-d <mazaridorian5@gmail.com>
2026-06-12 13:40:14 +0200

    file to handle the pages management

---
+++ src/pages.c
@@ -0,0 +1,76 @@
+#include "malloc.h"
+
+size_t  get_page_size(size_t size)
+{
+	size_t	allocations_size;
+	size_t	page_size;
+
+	if (size <= TINY)
+		allocations_size = 100 * TINY;
+	else if (size <= SMALL)
+		allocations_size = 100 * SMALL;
+	else if (size <= LARGE)
+		allocations_size = 100 * LARGE;
+	else
+		allocations_size = size;
+	page_size = getpagesize();
+	if (allocations_size % page_size)
+		size = page_size * ((allocations_size / page_size) + 1);
+	else
+		size = page_size * (allocations_size / page_size);
+	return size;
+}
+
+t_page	*create_page(size_t	size, int flag_ctx)
+{
+	void		*addr;
+	size_t	page_size;
+	size_t	offset;
+	t_page	*page;
+
+	page_size = get_page_size(size);
+	addr = mmap(NULL, page_size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (addr == (void*)-1)
+		return NULL;
+	page = addr;
+	page->next = NULL;
+	page->prev = NULL;
+	page->page_size = page_size;
+	offset = ALIGN_UP(sizeof(t_page), ALIGNMENT);
+	if (flag_ctx)
+		offset += set_context((char*)addr + offset);
+	page->first_chunk = (t_memory_chunk*)((char*)addr + offset);
+	page->first_chunk->is_free = true;
+	page->first_chunk->next = NULL;
+	page->first_chunk->prev = NULL;
+	page->first_chunk->page = addr;
+	offset += ALIGN_UP(sizeof(t_memory_chunk), ALIGNMENT);
+	page->first_chunk->size = page_size - (ALIGN_UP(sizeof(t_page), ALIGNMENT) + ALIGN_UP(sizeof(t_memory_chunk), ALIGNMENT));
+	return page;
+}
+
+void	add_page(t_page	*curr, t_page	*new)
+{
+	t_page	*prev;
+
+	prev = curr;
+	while (prev && prev->next)
+		prev = prev->next;
+	if (prev)
+		prev->next = new;
+	new->prev = prev;
+}
+
+t_page	*get_page_list(size_t size)
+{
+	t_ctx	*ctx;
+
+	ctx = get_context();
+	if (size <= TINY)
+		return ctx->tiny;
+	if (size <= SMALL)
+		return ctx->small;
+	if (size <= LARGE)
+		return ctx->large;
+	return ctx->others;
+}
